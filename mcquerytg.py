873f4014190c8218ec0d6ccb1849e3879161a0c5
h4rmy <75831703+h4rmy@users.noreply.github.com>
2022-05-01 22:06:59 +0500

    add non-query status lookup

--- mcquerytg.py
+++ mcquerytg.py
@@ -1,4 +1,5 @@
-import logging, time
+import logging, requests, json
+import socket
 from datetime import datetime
 from mcstatus import JavaServer
 from aiogram import Bot, Dispatcher, executor, types
@@ -8,12 +9,56 @@ API_TOKEN = 'tgapi'
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+whitelist = [555091019, 328508967, 615744908]
+            #h4rmy      crinny      slamerr
+
+@dp.message_handler(commands=['id'])
+async def welcome(message: types.Message):
+    await message.reply("<code>" + str(message.chat.id) + "</code>", parse_mode="HTML")
+
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
-    await message.reply("/srv [ip] [stat/plugins]")
+    now = datetime.now()
+    timelol = now.strftime("%H:%M:%S")
+    timelol = str(timelol)
+    print(f'[{timelol}] [@{message.from_user.username}] {message.from_user.first_name} started the bot')
+    await message.reply("/srv [ip/host]\n/q [ip/host] [stat/plugins]\n/id\n/owner")
+
+@dp.message_handler(lambda message: message.chat.id not in whitelist)
+async def checker(message):
+    now = datetime.now()
+    timelol = now.strftime("%H:%M:%S")
+    timelol = str(timelol)
+    print(f'[{timelol}] [@{message.from_user.username}] {message.from_user.first_name} tried to get access')
+    await message.answer_photo("https://github.com/h4rmy/filehosting/blob/main/2.png?raw=true")
+
+@dp.message_handler(commands=['owner'])
+async def welcome(message: types.Message):
+    await message.reply("<b>@h4rmy</b>", parse_mode="HTML")
 
 @dp.message_handler(commands=['srv'])
 async def server(message: types.Message):
+    args = message.get_args().split()
+    json = requests.get(f'https://api.mcsrvstat.us/2/{args[0]}').json()
+
+    hostname = json.get("hostname", "Not found")
+    ip = json["ip"]
+    version = json["version"]
+    software = json.get("software", "Vanilla")
+    motd0 = json["motd"]["clean"][0]
+    motd1 = json["motd"]["clean"][1]
+    motd = motd0 + "\n" + motd1
+    players = "\n - ".join(json["players"]["list"])
+    p = json["players"]["online"]
+    p1 = json["players"]["max"]
+    info = "\n".join(json["info"]["clean"])
+    query = json["debug"]["query"]
+
+    await message.answer(f' <b> [Hostname]</b>: {hostname} [{p}/{p1}]\n<b> [IP]</b>: {ip}\n<b> [Version]</b>: {version}\n<b> [Core]</b>: {software}\n<b> [Query]</b>: {query}\n<b> [MOTD]</b>:\n{motd}\n<b> [Info]</b>:\n {info}\n<b> [Players]</b>: \n - {players}\n', parse_mode="HTML")
+
+
+@dp.message_handler(commands=['q'])
+async def query(message: types.Message):
     try:
         args = message.get_args().split()
         if args[1] == "stat":
@@ -31,13 +76,15 @@ async def server(message: types.Message):
                 names = "\n- ".join(query.players.names)
 
                 n = "\n"
-                await message.answer(f'{host}:{port} [{status.players.online}/{status.players.max}]' + n +
-                                     f'ping: {latency}ms' + n +
-                                     f'players online:\n- {names}' + n +
-                                     f'version: {query.software.version}' + n +
-                                     f'brand: {query.software.brand}')
-            except:
-                await message.answer("не бумбум")
+                await message.answer(f'<b>{host}:{port}</b> [{status.players.online}/{status.players.max}]' + n +
+                                     f'<b>Ping</b>: {latency}ms' + n +
+                                     f'<b>Ip</b>: {query.raw["hostip"]}' + n +
+                                     f'<b>Version</b>: {query.software.version}' + n +
+                                     f'<b>Brand</b>: {query.software.brand}' + n +
+                                     f'<b>Map</b>: {query.raw["map"]}' + n +
+                                     f'<b>Players online</b>:\n- {names}', parse_mode="HTML")
+            except socket.timeout:
+                await message.answer_photo("https://github.com/h4rmy/filehosting/blob/main/3.png?raw=true")
         elif args[1] == "plugins":
             try:
                 now = datetime.now()
@@ -47,21 +94,22 @@ async def server(message: types.Message):
                 srv = JavaServer.lookup(args[0])
                 query = srv.query()
                 rawplugins = query.software.plugins
+                plcount = len(rawplugins)
                 plugins = [i.split()[0] for i in rawplugins]
                 try:
                     plugins.sort()
                     return plugins
                 finally:
                     plugins = "\n".join(plugins)
-                    await message.answer(plugins)
-            except:
-                await message.answer("не бумбум")
+                    await message.answer(plugins + "\n\n<b>Plugins</b>: " + str(plcount), parse_mode="HTML")
+            except socket.timeout:
+                await message.answer_photo("https://github.com/h4rmy/filehosting/blob/main/3.png?raw=true")
     except IndexError:
         now = datetime.now()
         timelol = now.strftime("%H:%M:%S")
         timelol = str(timelol)
         print(f'[{timelol}] [@{message.from_user.username}] {message.from_user.first_name} {args}')
-        await message.answer("no bitches?")
+        await message.answer_photo("https://github.com/h4rmy/filehosting/blob/main/1.png?raw=true")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
